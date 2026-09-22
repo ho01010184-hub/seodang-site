401a0621c3a710f955f9c1a1ad548b7aa1d91eed
임호균 <ho01010184@gmail.com>
2026-09-22 15:08:59 +0900

    웹앱 갱신 — Sentry 키 스크럽

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4241118376" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3277145063" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
