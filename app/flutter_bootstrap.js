7982c935f358a0d6c455b4fa8965c4fcfaa3527b
임호균 <ho01010184@gmail.com>
2026-09-23 13:17:56 +0900

    웹앱 갱신 — Zone mismatch 수정

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3424942853" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "870626000" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
